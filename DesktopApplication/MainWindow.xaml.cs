b7a57ae87da8f86c5a9d2e2d824629424e981f4b
crousap <timkolerov1@gmail.com>
2022-04-18 17:20:46 +0300

    Initial state

--- DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DesktopApplication
+namespace Application
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- DesktopApplication/Properties/Settings.Designer.cs
+++ DesktopApplication/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DesktopApplication.Properties
+namespace Application.Properties
 {
 
 
