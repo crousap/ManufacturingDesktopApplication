f3e3d9685e535fd01f297fc39bc64c536df6ac3f
crousap <timkolerov1@gmail.com>
2022-04-20 19:27:55 +0300

    Working login logic

---
+++ DesktopApplication/Classes/Authorizator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopApplication.DbModel;
+
+namespace DesktopApplication.Classes
+{
+    public static class Authorizator
+    {
+        public static User CurrentUser { get => _currentUser; }
+        public static Roles CurrentRole { get => _currentRole; }
+        public static bool IsAuthorized { get => _isAuthorized; }
+
+        private static User _currentUser;
+        private static Roles _currentRole;
+        private static bool _isAuthorized = false;
+
+        public static bool LoginCheck(string login, string password)
+        {
+            var result = manufacturingEntities.GetContext().LoginChecker(login, password).FirstOrDefault();
+            if (result != null)
+            {
+                _currentUser = manufacturingEntities.GetContext().Users.Where(x => x.Login == login).First();
+                _isAuthorized = true;
+                _currentRole = (Roles)CurrentUser.Role1.Id;
+            }
+            return IsAuthorized;
+        }
+    }
+
+    public enum Roles
+    {
+        Инженер = 1,
+        Менеджер,
+        Сварщик,
+        Складовщик,
+        Станочник,
+        Универсальный_рабочий
+    }
+}
--- DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DesktopApplication.DbModel;
+using DesktopApplication.Classes;
 
 namespace Application
 {
@@ -20,10 +23,27 @@ namespace Application
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var login = textBoxLogin.Text;
+            var password = passwordBox.Password;
+
+            var isLogged = Authorizator.LoginCheck(login, password);
+
+            if (isLogged)
+            {
+                var window = new DesktopApplication.Windows.PagesHolder();
+                window.Show();
+                this.Close();
+            }
+            else
+                MessageBox.Show("Не правильный логин или пароль");
+        }
     }
 }
