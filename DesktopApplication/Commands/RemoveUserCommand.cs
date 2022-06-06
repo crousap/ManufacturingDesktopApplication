d48027838930cec13a3d74e2c0465e4e4667cdb8
crousap <timkolerov1@gmail.com>
2022-06-06 18:14:36 +0300

    Добавил возможность удалять пользователей

---
+++ DesktopApplication/Commands/RemoveUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using DesktopApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopApplication.Commands
+{
+    public class RemoveUserCommand : CommandBase
+    {
+        private UserInfoFieldsViewModel _viewModel;
+
+        public RemoveUserCommand(UserInfoFieldsViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.RemoveUser();
+        }
+    }
+}
